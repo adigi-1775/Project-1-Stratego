3ac7f1bb76391fb2e8d36f0d4afd44f7b98efcc5
adigi-1775 <adigi1775@gmail.com>
2021-08-20 18:42:23 -0400

    game logic works

--- js/app.js
+++ js/app.js
@@ -5,11 +5,22 @@ let pieceSelection = null;
 let pieceToBeMoved = null;
 let timesClicked = 0;
 let turn = 'blue';
-
-// array of objects (game pieces)
-// const bluePieces = [{name: 'general', value:1}, {name:'colonel', value:2}, {name: 'major', value:3}, {name: 'captain', value:4}, {name: 'lieutenant', value:5}, {name: 'sergeant', value:6}, {name: 'corporal', value:7}, {name: 'mineSweeper', value:8}, {name: 'infantry', value:9}, {name: 'flag', value: 10}];
-//
-// const redPieces = [{name: 'general', value:1}, {name:'colonel', value:2}, {name: 'major', value:3}, {name: 'captain', value:4}, {name: 'lieutenant', value:5}, {name: 'sergeant', value:6}, {name: 'corporal', value:7}, {name: 'mineSweeper', value:8}, {name: 'infantry', value:9}, {name: 'flag', value: 10}];
+const bluePieces = document.querySelectorAll('.blue')
+const redPieces = document.querySelectorAll('.red')
+const blueWinsGame = ()=>{
+  alert('Blue Wins!')
+}
+const redWinsGame = ()=>{
+  alert('Red Wins!')
+}
+function losingRedPiece(redDead){
+  let moveToContainer = document.querySelector('.pieces-container-p2')
+      moveToContainer.appendChild(redDead)
+}
+function losingBluePiece(blueDead){
+  let moveToComtainer1 = document.querySelector('.pieces-container-p1')
+      moveToComtainer1.appendChild(blueDead)
+}
 
 // array of squares
 const cells = ['1A', '1B', '1C', '1D', '1E', '1F', '1G', '1H', '1I', '1J', '2A', '2B', '2C', '2D', '2E', '2F','2G', '2H', '2I', '2J', '3A', '3B','3C', '3D', '3E', '3F', '3G', '3H', '3I', '3J', '4A', '4B', '4C', '4D','4E', '4F', '4G', '4H', '4I', '4J', '5A', '5B', '5E', '5F', '5I', '5J', '6A', '6B', '6E', '6F', '6I', '6J', '7A', '7B', '7C', '7D', '7E', '7F', '7G', '7H', '7I', '7J', '8A', '8B', '8C', '8D', '8E', '8F', '8G', '8H', '8I', '8J', '9A', '9B', '9C', '9D', '9E', '9F', '9G', '9H', '9I', '9J', '10A', '10B', '10C', '10D', '10E', '10F', '10G', '10H', '10I', '10J']
@@ -60,32 +71,44 @@ const game = {
         spaceBeingMovedTo.append(pieceToBeMoved)
         timesClicked = 0;
         pieceToBeMoved = null;
-        this.checkForBattle(spaceBeingMovedTo);
+        this.battleResults(spaceBeingMovedTo);
       }
     })
   },
-  checkForBattle(space){
+  battleResults(space){
     if(space.children.length === 2){
-      if(space.children[0].getAttribute('value') < space.children[1].getAttribute('value')){
-        space.children[1].append('.pieces-container-p2')
-      }else if(space.children[0].getAttribute('value') > space.children[1].getAttribute('value')){
-        space.children[0].append('.pieces-container-p1')
-      }
-      console.log(space.children[0].getAttribute('value'));
-      console.log(space.children[1].getAttribute('value'));
+        if(space.children[0].getAttribute('class') === 'blue' && space.children[0].getAttribute('value') < space.children[1].getAttribute('value')){
+            console.log(space.children[1]);
+            console.log('piece1');
+            losingRedPiece(space.children[1])
 
-      console.log('battle');
-    }else{
-      console.log('boring');
-    }
-  }
+        }else if(space.children[1].getAttribute('class') === 'blue' && space.children[1].getAttribute('value') < space.children[0].getAttribute('value')){
+            losingRedPiece(space.children[0])
+
+        }else if(space.children[0].getAttribute('class') === 'red' && space.children[0].getAttribute('value') < space.children[1].getAttribute('value')){
+            losingBluePiece(space.children[1])
+
+        }else if(space.children[1].getAttribute('class') === 'red' && space.children[1].getAttribute('value') < space.children[0].getAttribute('value')){
+            losingBluePiece(space.children[0])
+
+        }else if(space.children[0].getAttribute('class') === 'blue' && space.children[0].getAttribute('value') === space.children[1].getAttribute('value')){
+            losingRedPiece(space.children[1])
+            losingBluePiece(space.children[0])
+
+        }else if(space.children[1].getAttribute('class') === 'blue' && space.children[1].getAttribute('value') === space.children[0].getAttribute('value')){
+            losingBluePiece(space.children[1])
+            losingRedPiece(space.children[0])
+        }
+
+        console.log('battle');
+        }else if(space.children[0].getAttribute('#flag') && space.children[1].getAttribute('.red')){
+          redWinsGame()
+        }else if(space.children[0].getAttribute('#flagr') && space.children[1].getAttribute('.blue')){
+          blueWinsGame()
+        }
+      }
 }
 
-const bluePieces = document.querySelectorAll('.blue')
-const redPieces = document.querySelectorAll('.red')
-
-
-// console.log(document.getElementById("5C"));
 // document.getElementById("5C").style.pointerEvents = "none";
 // document.getElementById("5D").style.pointerEvents = "none";
 // document.getElementById("5G").style.pointerEvents = "none";
@@ -126,6 +149,11 @@ let turns = document.getElementById('turn-button')
 
 //----------------- ALL CODE BELOW IS FOR FUTURE UPGRADES ------------------//
 
+
+// array of objects (game pieces)
+// const bluePieces = [{name: 'general', value:1}, {name:'colonel', value:2}, {name: 'major', value:3}, {name: 'captain', value:4}, {name: 'lieutenant', value:5}, {name: 'sergeant', value:6}, {name: 'corporal', value:7}, {name: 'mineSweeper', value:8}, {name: 'infantry', value:9}, {name: 'flag', value: 10}];
+//
+// const redPieces = [{name: 'general', value:1}, {name:'colonel', value:2}, {name: 'major', value:3}, {name: 'captain', value:4}, {name: 'lieutenant', value:5}, {name: 'sergeant', value:6}, {name: 'corporal', value:7}, {name: 'mineSweeper', value:8}, {name: 'infantry', value:9}, {name: 'flag', value: 10}];
 
 // const pieceVs = {
 //         general: {
