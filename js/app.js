943401440e974028c63196439d30ca7d7ca831ad
adigi-1775 <adigi1775@gmail.com>
2021-08-20 19:17:02 -0400

    win alert works

--- js/app.js
+++ js/app.js
@@ -22,9 +22,6 @@ function losingBluePiece(blueDead){
       moveToComtainer1.appendChild(blueDead)
 }
 
-// array of squares
-const cells = ['1A', '1B', '1C', '1D', '1E', '1F', '1G', '1H', '1I', '1J', '2A', '2B', '2C', '2D', '2E', '2F','2G', '2H', '2I', '2J', '3A', '3B','3C', '3D', '3E', '3F', '3G', '3H', '3I', '3J', '4A', '4B', '4C', '4D','4E', '4F', '4G', '4H', '4I', '4J', '5A', '5B', '5E', '5F', '5I', '5J', '6A', '6B', '6E', '6F', '6I', '6J', '7A', '7B', '7C', '7D', '7E', '7F', '7G', '7H', '7I', '7J', '8A', '8B', '8C', '8D', '8E', '8F', '8G', '8H', '8I', '8J', '9A', '9B', '9C', '9D', '9E', '9F', '9G', '9H', '9I', '9J', '10A', '10B', '10C', '10D', '10E', '10F', '10G', '10H', '10I', '10J']
-
 const blueCells = ['1A', '1B', '1C', '1D', '1E', '1F', '1G', '1H', '1I', '1J', '2A', '2B', '2C', '2D', '2E', '2F','2G', '2H', '2I', '2J', '3A', '3B','3C', '3D', '3E', '3F', '3G', '3H', '3I', '3J', '4A', '4B', '4C', '4D','4E', '4F', '4G', '4H', '4I', '4J']
 
 const redCells = ['7A', '7B', '7C', '7D', '7E', '7F', '7G', '7H', '7I', '7J', '8A', '8B', '8C', '8D', '8E', '8F', '8G', '8H', '8I', '8J', '9A', '9B', '9C', '9D', '9E', '9F', '9G', '9H', '9I', '9J', '10A', '10B', '10C', '10D', '10E', '10F', '10G', '10H', '10I', '10J']
@@ -77,9 +74,12 @@ const game = {
   },
   battleResults(space){
     if(space.children.length === 2){
-        if(space.children[0].getAttribute('class') === 'blue' && space.children[0].getAttribute('value') < space.children[1].getAttribute('value')){
-            console.log(space.children[1]);
-            console.log('piece1');
+        if(space.children[0].getAttribute('id') === 'flag' && space.children[1].getAttribute('class') === 'red'){
+          redWinsGame()
+        }else if(space.children[0].getAttribute('id') === 'flagr' && space.children[1].getAttribute('class') === 'blue'){
+          blueWinsGame()
+          
+        }else if(space.children[0].getAttribute('class') === 'blue' && space.children[0].getAttribute('value') < space.children[1].getAttribute('value')){
             losingRedPiece(space.children[1])
 
         }else if(space.children[1].getAttribute('class') === 'blue' && space.children[1].getAttribute('value') < space.children[0].getAttribute('value')){
@@ -99,24 +99,18 @@ const game = {
             losingBluePiece(space.children[1])
             losingRedPiece(space.children[0])
         }
-
-        console.log('battle');
-        }else if(space.children[0].getAttribute('#flag') && space.children[1].getAttribute('.red')){
-          redWinsGame()
-        }else if(space.children[0].getAttribute('#flagr') && space.children[1].getAttribute('.blue')){
-          blueWinsGame()
-        }
       }
+    }
 }
 
-// document.getElementById("5C").style.pointerEvents = "none";
-// document.getElementById("5D").style.pointerEvents = "none";
-// document.getElementById("5G").style.pointerEvents = "none";
-// document.getElementById("5H").style.pointerEvents = "none";
-// document.getElementById("6C").style.pointerEvents = "none";
-// document.getElementById("6D").style.pointerEvents = "none";
-// document.getElementById("6G").style.pointerEvents = "none";
-// document.getElementById("6H").style.pointerEvents = "none";
+document.getElementById("5C").style.pointerEvents = "none";
+document.getElementById("5D").style.pointerEvents = "none";
+document.getElementById("5G").style.pointerEvents = "none";
+document.getElementById("5H").style.pointerEvents = "none";
+document.getElementById("6C").style.pointerEvents = "none";
+document.getElementById("6D").style.pointerEvents = "none";
+document.getElementById("6G").style.pointerEvents = "none";
+document.getElementById("6H").style.pointerEvents = "none";
 
 // event listeners
 let form = document.getElementById('place-piece-button')
@@ -149,6 +143,8 @@ let turns = document.getElementById('turn-button')
 
 //----------------- ALL CODE BELOW IS FOR FUTURE UPGRADES ------------------//
 
+// array of squares
+// const cells = ['1A', '1B', '1C', '1D', '1E', '1F', '1G', '1H', '1I', '1J', '2A', '2B', '2C', '2D', '2E', '2F','2G', '2H', '2I', '2J', '3A', '3B','3C', '3D', '3E', '3F', '3G', '3H', '3I', '3J', '4A', '4B', '4C', '4D','4E', '4F', '4G', '4H', '4I', '4J', '5A', '5B', '5E', '5F', '5I', '5J', '6A', '6B', '6E', '6F', '6I', '6J', '7A', '7B', '7C', '7D', '7E', '7F', '7G', '7H', '7I', '7J', '8A', '8B', '8C', '8D', '8E', '8F', '8G', '8H', '8I', '8J', '9A', '9B', '9C', '9D', '9E', '9F', '9G', '9H', '9I', '9J', '10A', '10B', '10C', '10D', '10E', '10F', '10G', '10H', '10I', '10J']
 
 // array of objects (game pieces)
 // const bluePieces = [{name: 'general', value:1}, {name:'colonel', value:2}, {name: 'major', value:3}, {name: 'captain', value:4}, {name: 'lieutenant', value:5}, {name: 'sergeant', value:6}, {name: 'corporal', value:7}, {name: 'mineSweeper', value:8}, {name: 'infantry', value:9}, {name: 'flag', value: 10}];
